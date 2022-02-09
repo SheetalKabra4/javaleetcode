db03cb1169e5737c2e5f607d57becdeee1cb9244
Sheetal <sheetal@Sheetals-MacBook-Pro.local>
2022-02-09 14:50:48 +0530

    binary search with test cases

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -4,37 +4,32 @@ import java.util.List;
 
 public class BinarySearch {
 
-    public static void main(String[] args) throws InvalidInputException {
-        BinarySearch binarySearch = new BinarySearch();
-        List<Integer> list = Arrays.asList(2,8,9,13,28,21,23,45,50);
-        binarySearch.binarySearchUsingArrayList(list, 45);
-    }
-    public void binarySearchUsingArrayList(List<Integer> list, int searchItem) throws InvalidInputException{
-        if(list.size() == 0 || list == null){
+//    public static void main(String[] args) throws InvalidInputException {
+//        BinarySearch bs = new BinarySearch();
+//        List<Integer> list = Arrays.asList(2,8,9,13,28,21,23,45,50);
+//        bs.binarySearchUsingArrayList(null, 45);
+//    }
+    public int  binarySearchUsingArrayList(List<Integer> list, int searchItem) throws InvalidInputException{
+        if(list == null || list.size() == 0){
             throw new InvalidInputException();
         }
         int l = 0;
         int r = list.size()-1;
-        int itemPlace = binarySearch(list, l, r, searchItem);
-        System.out.println(itemPlace);
-        //return itemPlace;
+        int itemPlace = this.binarySearch(list, l, r, searchItem);
+        return itemPlace;
     }
 
     public int binarySearch(List<Integer> list, int l, int r, int searchItem){
         int m = (l+r)/2;
-        System.out.println(m);
         while(r>=l){
             if(list.get(m) == searchItem){
-                System.out.println("m");
                 return m;
             }else if(list.get(m) > searchItem){
                 //goto lhs
-                System.out.println("lhs");
-                binarySearch(list, l, m-1, searchItem);
+                return binarySearch(list, l, m-1, searchItem);
             }else if(list.get(m) < searchItem){
                 //goto rhs
-                System.out.println("rhs");
-                binarySearch(list, m+1, r, searchItem);
+                return binarySearch(list, m+1, r, searchItem);
             }
         }
         return -1;
--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -8,10 +8,98 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTest {
     BinarySearch binarySearch = new BinarySearch();
+
     @Test
-    void binarySearchUsingArrayList() throws InvalidInputException{
-//        List<Integer> list = Arrays.asList(2,8,9,13,28,21,23,45,50);
-//        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 45);
-//        assertEquals(7, itemPlace);
+    void binarySearchUsingArrayList_Null() throws InvalidInputException{
+        //int itemPlace = binarySearch.binarySearchUsingArrayList(list, 45);
+        assertThrows(InvalidInputException.class, () -> binarySearch.binarySearchUsingArrayList(null, 45));
     }
+
+    @Test
+    void binarySearchUsingArrayList_Empty() throws InvalidInputException{
+        List<Integer> list = new ArrayList<>();
+        assertThrows(InvalidInputException.class, () -> binarySearch.binarySearchUsingArrayList(list, 45));
+    }
+
+    @Test
+    void binarySearchUsingArrayList_NotFound1() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,3,5,9,21,98,101);
+        int item = binarySearch.binarySearchUsingArrayList(list, 20);
+        assertEquals(-1, item);
+    }
+    @Test
+    void binarySearchUsingArrayList_NotFound2() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 65);
+        assertEquals(-1, itemPlace);
+    }
+    @Test
+    void binarySearchUsingArrayList_NotFound3() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 1);
+        assertEquals(-1, itemPlace);
+    }
+    @Test
+    void binarySearchUsingArrayList_NotFound4() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 7);
+        assertEquals(-1, itemPlace);
+    }
+
+    @Test
+    void binarySearchUsingArrayList1() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 2);
+        assertEquals(0, itemPlace);
+    }
+
+    @Test
+    void binarySearchUsingArrayList2() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 8);
+        assertEquals(1, itemPlace);
+    }
+    @Test
+    void binarySearchUsingArrayList3() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 9);
+        assertEquals(2, itemPlace);
+    }
+    @Test
+    void binarySearchUsingArrayList4() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 13);
+        assertEquals(3, itemPlace);
+    }
+    @Test
+    void binarySearchUsingArrayList5() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 20);
+        assertEquals(4, itemPlace);
+    }
+    @Test
+    void binarySearchUsingArrayList6() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 21);
+        assertEquals(5, itemPlace);
+    }
+    @Test
+    void binarySearchUsingArrayList7() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 23);
+        assertEquals(6, itemPlace);
+    }
+    @Test
+    void binarySearchUsingArrayList8() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 45);
+        assertEquals(7, itemPlace);
+    }
+    @Test
+    void binarySearchUsingArrayList9() throws InvalidInputException{
+        List<Integer> list = Arrays.asList(2,8,9,13,20,21,23,45,50);
+        int itemPlace = binarySearch.binarySearchUsingArrayList(list, 50);
+        assertEquals(8, itemPlace);
+    }
+
 }
